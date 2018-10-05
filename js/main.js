1f013297ea8dc3aa485dd690e2cd8fd4b0e96e87
John Wu <molvqingtai@gmail.com>
2018-10-05 20:30:49 +0800

    fix bug

--- js/main.js
+++ js/main.js
@@ -25,13 +25,19 @@
                             iconsBtn.lastElementChild.style.display = 'none'
                             downloadBtn.setAttribute('disabled', 'disabled')
                             downloadBtn.textContent = '生成水印中...'
-                            drawing(res, e.target.result, (url) => {
-                                displayImg.src = downloadBtn.href = url
-                                displayImg.onload = () => {
-                                    downloadBtn.download = file.name.slice(0, file.name.lastIndexOf('.')) + '.jpeg'
+                            drawing(res, e.target.result).then((res) => {
+                                if (res) {
+                                    displayImg.src = downloadBtn.href = res
+                                    displayImg.onload = () => {
+                                        downloadBtn.download = file.name.slice(0, file.name.lastIndexOf('.')) + '.jpeg'
+                                        iconsBtn.firstElementChild.style.display = 'none'
+                                        iconsBtn.lastElementChild.style.display = 'block'
+                                        downloadBtn.removeAttribute('disabled')
+                                        downloadBtn.textContent = '下载水印照片'
+                                    }
+                                } else {
                                     iconsBtn.firstElementChild.style.display = 'none'
                                     iconsBtn.lastElementChild.style.display = 'block'
-                                    downloadBtn.removeAttribute('disabled')
                                     downloadBtn.textContent = '下载水印照片'
                                 }
                             })
@@ -48,38 +54,44 @@
 
     }
     let drawing = (exif, base64, callback) => {
-        let canvas = document.createElement('canvas')
-        let context = canvas.getContext('2d')
-        let image = new Image()
-        let logo = new Image()
-        let dx = 0
-        let dy = 0
-        let dw = exif.PixelXDimension
-        let dh = exif.PixelYDimension
-        let sx = exif.PixelXDimension * 0.02
-        let sy = exif.PixelYDimension * 0.02
-        let sw = exif.PixelXDimension * 0.96
-        let sh = exif.PixelYDimension * 0.96
-        let fx = exif.PixelXDimension * 0.04
-        let fy = exif.PixelYDimension * 1.04
-        canvas.width = exif.PixelXDimension
-        canvas.height = exif.PixelYDimension + exif.PixelYDimension * 0.12
-        image.onload = () => {
-            context.fillStyle = '#FFFFFF'
-            context.fillRect(0, 0, canvas.width, canvas.height)
-            context.drawImage(image, dx, dy, dw, dh, sx, sy, sw, sh)
-            // 加载字体
-            document.fonts.load('100px Smartisan').then(() => {
-                context.font = '900 100px sans-serif,Smartisan'
-                context.fillStyle = '#4B5F64'
-                context.fillText(`\ue900  ${exif.Model}`, fx, fy)
-                canvas.toBlob((blob) => {
-                    callback(URL.createObjectURL(blob))
-                }, 'image/jpeg', 1.0)
-            })
-        }
-        image.src = base64
-
+        return new Promise((resolve, reject) => {
+            let canvas = document.createElement('canvas')
+            let context = canvas.getContext('2d')
+            let image = new Image()
+            let logo = new Image()
+            let dx = 0
+            let dy = 0
+            let dw = exif.PixelXDimension
+            let dh = exif.PixelYDimension
+            let sx = exif.PixelXDimension * 0.02
+            let sy = exif.PixelYDimension * 0.02
+            let sw = exif.PixelXDimension * 0.96
+            let sh = exif.PixelYDimension * 0.96
+            let fx = exif.PixelXDimension * 0.04
+            let fy = exif.PixelYDimension * 1.04
+            canvas.width = exif.PixelXDimension
+            canvas.height = exif.PixelYDimension + exif.PixelYDimension * 0.12
+            image.onload = () => {
+                context.fillStyle = '#FFFFFF'
+                context.fillRect(0, 0, canvas.width, canvas.height)
+                context.drawImage(image, dx, dy, dw, dh, sx, sy, sw, sh)
+                // 加载字体
+                document.fonts.load('100px Smartisan').then(() => {
+                    context.font = '900 100px sans-serif,Smartisan'
+                    context.fillStyle = '#4B5F64'
+                    context.fillText(`\ue900  ${exif.Model}`, fx, fy)
+                    canvas.toBlob((blob) => {
+                        try {
+                            resolve(URL.createObjectURL(blob))
+                        } catch (e) {
+                            toastTip('照片EXIF信息不完整!')
+                            resolve(false)
+                        }
+                    }, 'image/jpeg', 1.0)
+                })
+            }
+            image.src = base64
+        })
     }
     let getEXIF = (file) => {
         return new Promise((resolve, reject) => {
@@ -96,8 +108,6 @@
             classes: 'rounded'
         })
     }
-
-
 
     let fullscreen = (e) => {
         let requestFullscreen = () => {
